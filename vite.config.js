d9c2f743a5f08142471a186088c1d63c149763e9
Dauran Lima <dauranlima@gmail.com>
2024-12-17 19:44:47 -0300

    commit: fixing tailwind import

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginRequire from 'vite-plugin-require';
 import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginRequire.default()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
